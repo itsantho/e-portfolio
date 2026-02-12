07c4ced79082c2c4c2e9347059ec86cbdabcf63a
itsantho <iem.anthony01@gmail.com>
2026-02-12 16:33:28 +0100

    download cv

--- src/components/OpenCV.vue
+++ src/components/OpenCV.vue
@@ -1,9 +1,14 @@
 <script setup lang="ts">
-
+const downloadCV = () => {
+  const link = document.createElement('a');
+  link.href = new URL('../assets/CV_dev_fullstack_Anthony_IEM.pdf', import.meta.url).href;
+  link.download = 'CV_dev_fullstack_Anthony_IEM.pdf';
+  link.click();
+};
 </script>
 
 <template>
-  <v-dialog max-width="500">
+  <v-dialog max-width="900">
     <template v-slot:activator="{ props: activatorProps }" >
       <v-btn
         v-bind="activatorProps"
@@ -21,8 +26,14 @@
 
     <template v-slot:default="{ isActive }">
       <v-card>
-        <v-img src="../assets/CV_dev_Anthony_IEM.jpg"></v-img>
+        <v-img  src="../assets/CV_dev_Anthony_IEM.jpg"></v-img>
         <v-card-actions>
+          <v-btn
+            text="Télécharger"
+            variant="flat"
+            color="surface-variant"
+            @click="downloadCV"
+          ></v-btn>
           <v-spacer></v-spacer>
           <v-btn
             text="Fermer"
